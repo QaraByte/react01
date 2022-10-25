a09423d02a606990f1d2ab40186a928206a5b2c6
QaraByte <pchelpkz16@gmail.com>
2022-10-25 20:34:09 +0600

    1) Components

---
+++ src/Body.jsx
@@ -0,0 +1,14 @@
+// import React from 'react';
+
+let Body=()=> {
+    return (
+      <div className="body">
+        <li>Html</li>
+        <li>CSS</li>
+        <li>JS</li>
+        <li>React</li>
+      </div>
+    );
+  }
+
+  export default Body;
---
+++ src/Header.jsx
@@ -0,0 +1,13 @@
+//import 
+
+let Header=()=> {
+    return (
+      <div className="header">
+        <a href="#">Home</a>
+        <a href="#">About us</a>
+        <a href="#">Contacts</a>
+      </div>
+    );
+  }
+
+  export default Header;
